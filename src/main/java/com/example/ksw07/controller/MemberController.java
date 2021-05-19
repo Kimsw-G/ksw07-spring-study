bb8ce2c559e2476ec2aeb61d463ee590d532c558
Kimsw-G <kimsw3445@naver.com>
2021-05-19 15:39:02 +0900

    link jsp

--- src/main/java/com/example/ksw07/controller/MemberController.java
+++ src/main/java/com/example/ksw07/controller/MemberController.java
@@ -1,9 +1,16 @@
 package com.example.ksw07.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
 
-//    @Resource(name="")
+    @RequestMapping("/hello")
+    public String sayHello(Model model){
+        model.addAttribute("hello","안녕하세용");
+        return "hello";
+    }
 }
